0b9ddf668e19e8e4ee2fea49d46abedd04320348
Imran Sayyed <imran.sayyed@gslab.com>
2019-07-15 11:30:07 +0530

    routing day 10 commit

---
+++ day-9/src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from '../servers.service';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+
+  server: {id:number, name:string, status:string};
+  serverName = '';
+  serverStatus = '';
+
+  constructor(private serverService:ServersService) { }
+
+  ngOnInit() {
+    this.server = this.serverService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serverService.updateServer({
+      id: this.server.id,
+      name: this.serverName,
+      status:this.serverStatus
+    });
+  }
+
+}
